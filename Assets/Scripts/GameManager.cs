3d720ec4be0b50b80553ad9fb23d7caaad36df31
funkykong1 <vilho.valkeala@gmail.com>
2023-09-14 12:32:05 +0300

    tmp + gm start

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public List<GameObject> items;
+    int score;
+    private float spawnRate;
+    public TextMeshProUGUI scoreText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
+        UpdateScore(0);
+        StartCoroutine(SpawnTarget());
+
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnTarget()
     {
-        
+        while (true) {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, items.Count);
+            Instantiate(items[index]);
+
+        }
+    }
+
+    private void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
