598711885f18713ac1d193528076ae1c1260b46d
funkykong1 <vilho.valkeala@gmail.com>
2023-10-05 11:44:16 +0300

    necessary fix

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.Audio;
+
 public class AudioManager : MonoBehaviour
 {
     AudioSource source, musicSource;
